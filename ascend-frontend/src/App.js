83ebb0bdcee80689c84e4fbefebf3bbf1ecd688a
majok31 <majoktot70@gmail.com>
2025-01-08 22:38:07 +0300

    Ascend dependencies

--- ascend-frontend/src/App.js
+++ ascend-frontend/src/App.js
@@ -1,41 +1,48 @@
-import React from 'react';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Signup from './components/Signup';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Courses from './pages/Courses';
-import Logout from './pages/Logout';
-import { Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Courses from './components/pages/Courses';
+import CourseContent from './components/pages/CourseContent';
+import MyAccount from './components/pages/MyAccount';
+import CreateCourse from './components/instructor/pages/CreateCourse';
+import AddVideo from './components/instructor/pages/AddVideo';
+import MyCourses from './components/instructor/pages/MyCourses';
+import CourseVideos from './components/instructor/pages/CourseVideos';
+import CourseVideo from './components/instructor/pages/CourseVideo';
+import QnA from './components/QnA';
+import './App.css';
 
-function Home() {
+const App = () => {
   return (
-    <div>
-      <h1>Welcome to Ascend!</h1>
-      <nav>
-        <ul>
-        <li><Link to="/signup">Sign Up</Link></li>
-        <li><Link to="/login">Log In</Link></li>
-        <li><Link to="/courses">Courses</Link></li>
-        <li><Link to="/dashboard">Dashboard</Link></li>
-        </ul>
-      </nav>
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/course_content" element={<CourseContent />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/myaccount" element={<MyAccount />} />
+
+          {/* Instructor Routes */}
+          <Route path="/instructor/mycourses" element={<MyCourses />} />
+          <Route path="/instructor/create_course" element={<CreateCourse />} />
+          <Route path="/instructor/course_videos" element={<CourseVideos />} />
+          <Route path="/instructor/course_video" element={<CourseVideo />} />
+          <Route path="/instructor/add_video" element={<AddVideo />} />
+
+          <Route path="/qna" element={<QnA />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
-}
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/logout" element={<Logout />} />
-      </Routes>
-    </Router>
-  );
-}
+};
 
 export default App;
